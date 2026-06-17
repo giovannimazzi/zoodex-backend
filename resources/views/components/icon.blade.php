3c127aa8e6c8785ee13ae9d80f43690d67590ad6
giovannimazzi <gioelor@libero.it>
2026-06-18 00:41:04 +0200

    add icon component

---
+++ resources/views/components/icon.blade.php
@@ -0,0 +1,13 @@
+@props(['entity', 'measure', 'shape'])
+
+@if ($entity->image)
+    <img src="{{ asset('storage/' . $entity->image) }}"
+        alt="{{ $entity->name }}"
+        style="
+            width: {{ $measure }}px;
+            height: {{ $measure }}px;
+            object-fit: contain;
+            border-radius: {{ $shape == 1 ? '50%' : '0%'}};
+            background-color: {{ $entity->color }};
+        ">
+@endif
