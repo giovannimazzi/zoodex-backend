17c4fbcdea47a5f53f1953c7ee9cfef78ff8a433
giovannimazzi <gioelor@libero.it>
2026-06-18 00:46:11 +0200

    add button link in homepage

--- resources/views/admin/homepage.blade.php
+++ resources/views/admin/homepage.blade.php
@@ -5,6 +5,9 @@
     <div class="container py-4">
         <h1>{{ __('Area Amministrativa') }}</h1>
         <h2>{{ __('Benvenuto') }}, {{ ucfirst($user->name) }}!</h2>
+        <a href="{{ route('admin.animals.index') }}" class="btn btn-lg btn-primary mt-3">
+            Consulta ZooDex
+        </a>
     </div>
 
 @endsection
