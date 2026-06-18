1c38c4e4d1baa864334f1217562ff07264fa0656
giovannimazzi <gioelor@libero.it>
2026-06-18 18:46:24 +0200

    add pagination custom style

---
+++ resources/views/vendor/pagination/bootstrap-5.blade.php
@@ -0,0 +1,73 @@
+@if ($paginator->hasPages())
+    <nav aria-label="Paginazione">
+        <ul class="pagination justify-content-center flex-wrap mb-0">
+
+            {{-- Precedente --}}
+            @if ($paginator->onFirstPage())
+                <li class="page-item disabled">
+                    <span class="page-link">
+                        <i class="bi bi-chevron-left"></i>
+                    </span>
+                </li>
+            @else
+                <li class="page-item">
+                    <a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev">
+                        <i class="bi bi-chevron-left"></i>
+                    </a>
+                </li>
+            @endif
+
+            {{-- Numeri pagina: visibili da tablet in su --}}
+            <div class="d-none d-sm-flex">
+                @foreach ($elements as $element)
+
+                    @if (is_string($element))
+                        <li class="page-item disabled">
+                            <span class="page-link">{{ $element }}</span>
+                        </li>
+                    @endif
+
+                    @if (is_array($element))
+                        @foreach ($element as $page => $url)
+                            @if ($page == $paginator->currentPage())
+                                <li class="page-item active">
+                                    <span class="page-link">{{ $page }}</span>
+                                </li>
+                            @else
+                                <li class="page-item">
+                                    <a class="page-link" href="{{ $url }}">
+                                        {{ $page }}
+                                    </a>
+                                </li>
+                            @endif
+                        @endforeach
+                    @endif
+
+                @endforeach
+            </div>
+
+            {{-- Info mobile --}}
+            <li class="page-item disabled d-sm-none">
+                <span class="page-link">
+                    {{ $paginator->currentPage() }} / {{ $paginator->lastPage() }}
+                </span>
+            </li>
+
+            {{-- Successiva --}}
+            @if ($paginator->hasMorePages())
+                <li class="page-item">
+                    <a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next">
+                        <i class="bi bi-chevron-right"></i>
+                    </a>
+                </li>
+            @else
+                <li class="page-item disabled">
+                    <span class="page-link">
+                        <i class="bi bi-chevron-right"></i>
+                    </span>
+                </li>
+            @endif
+
+        </ul>
+    </nav>
+@endif
