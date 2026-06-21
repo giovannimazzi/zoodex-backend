80703ca5a10349a73bb0214b7c3d4d0729187ae0
giovannimazzi <gioelor@libero.it>
2026-06-21 23:51:53 +0200

    add rememberCollapse function

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,3 +3,23 @@ import "./bootstrap";
 import "~icons/bootstrap-icons.scss";
 import * as bootstrap from "bootstrap";
 import.meta.glob(["../img/**"]);
+
+window.rememberCollapse = function (collapseId, storageKey) {
+    const panel = document.getElementById(collapseId);
+
+    if (!panel) return;
+
+    const savedState = localStorage.getItem(storageKey);
+
+    if (savedState === "true") {
+        panel.classList.add("show");
+    }
+
+    panel.addEventListener("shown.bs.collapse", function () {
+        localStorage.setItem(storageKey, "true");
+    });
+
+    panel.addEventListener("hidden.bs.collapse", function () {
+        localStorage.setItem(storageKey, "false");
+    });
+};
