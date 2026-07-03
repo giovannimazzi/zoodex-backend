a4763d4538abce884815373ac330c6038fe1f2ef
giovannimazzi <gioelor@libero.it>
2026-07-03 19:44:17 +0200

    add logos asset and seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $logoSourcePath = database_path('seeders/assets/logos/logo-zoodex.png');
+        $heroSourcePath = database_path('seeders/assets/logos/hero-zoodex.png');
+
+        if (File::exists($logoSourcePath)) {
+            Storage::disk('public')->put('logo-zoodex.png', File::get($logoSourcePath));
+        }
+
+        if (File::exists($heroSourcePath)) {
+            Storage::disk('public')->put('hero-zoodex.png', File::get($heroSourcePath));
+        }
+
         $this->call([
             AnimalClassSeeder::class, 
             DietSeeder::class, 
