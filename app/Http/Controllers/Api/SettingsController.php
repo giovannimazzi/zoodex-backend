33f340eff000bf07d6975d9fa552ea285ede52a2
giovannimazzi <gioelor@libero.it>
2026-07-10 00:32:51 +0200

    fix settings api

--- app/Http/Controllers/Api/SettingsController.php
+++ app/Http/Controllers/Api/SettingsController.php
@@ -10,27 +10,13 @@ class SettingsController extends Controller
     public function index()
     {
         return response()->json([
-
             'success' => true,
-
             'results' => [
-
-                'app_name' => 'ZooDex',
-
-                'hero_title' => 'ZooDex',
-
-                'hero_subtitle_en' => 'Know Them All',
-
-                'hero_subtitle_it' => 'Conoscili tutti',
-
-                'navbar_logo' => asset('storage/logo-zoodex.png'),
-
-                'hero_logo' => asset('storage/hero-zoodex.png'),
-
+                'app_name' => config('app.name', 'ZooDex'),
+                'navbar_logo' => asset(...),
+                'hero_logo' => asset(...),
                 'animals_count' => Animal::count(),
-
-            ]
-
+            ],
         ]);
     }
 }
